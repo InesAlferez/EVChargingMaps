d047725f555afca47288b59cd26646209c562f5c
InesAlferez <ialferez1@live.maryville.edu>
2022-12-10 22:15:05 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'EVChargingMaps',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
